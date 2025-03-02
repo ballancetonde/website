ff2555c3d4844bbcafbacfc8061d78a906b1bf3d
Maxime Pauvert <maximepvrt@gmail.com>
2025-03-02 18:49:27 +0100

    Update index.vue

--- app/pages/evenements/index.vue
+++ app/pages/evenements/index.vue
@@ -1,6 +1,9 @@
 <script setup>
-const { data: posts } = await useAsyncData(useRoute().path, () => {
-  return queryCollection('events').order('date', 'DESC').all()
+const { data: nextEvents } = await useAsyncData(useRoute().path + '-next', () => {
+  return queryCollection('events').where('date', '>', new Date().toISOString()).order('date', 'ASC').all()
+})
+const { data: previousEvents } = await useAsyncData(useRoute().path + '-prev', () => {
+  return queryCollection('events').where('date', '<', new Date().toISOString()).order('date', 'DESC').all()
 })
 </script>
 
@@ -15,18 +18,32 @@ const { data: posts } = await useAsyncData(useRoute().path, () => {
     <UPageBody>
       <UBlogPosts>
         <UBlogPost
-            v-for="(post, index) in posts"
+            v-for="(post, index) in nextEvents"
             :key="index"
             :description="post.address"
             :image="post.image"
             :title="new Date(post.date).toLocaleDateString('fr', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric' })"
-            :orientation="index === 0 ? 'horizontal' : 'vertical'"
-            :class="[index === 0 && 'col-span-full']"
+            :orientation="'horizontal'"
+            class="col-span-full"
             :ui="{
             description: 'line-clamp-2'
           }"
         />
       </UBlogPosts>
+      <UPageSection title="Événements passées">
+        <UBlogPosts>
+          <UBlogPost
+              v-for="(post, index) in previousEvents"
+              :key="index"
+              :description="post.address"
+              :image="post.image"
+              :title="new Date(post.date).toLocaleDateString('fr', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric' })"
+              :ui="{
+            description: 'line-clamp-2'
+          }"
+          />
+        </UBlogPosts>
+      </UPageSection>
     </UPageBody>
   </UContainer>
 </template>
