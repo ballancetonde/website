4a7655687faba57d6d8cbf56ca92117eef07d7f6
Maxime Pauvert <maximepvrt@gmail.com>
2025-12-27 16:17:24 +0100

    update cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-12-27',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: [
@@ -29,6 +29,13 @@ export default defineNuxtConfig({
       },
       i18n: {
           defaultLocale: 'fr'
+      }
+  },
+  nitro: {
+      preset: "cloudflare_module",
+      cloudflare: {
+          deployConfig: true,
+          nodeCompat: true
       }
   }
 })
